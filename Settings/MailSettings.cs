73e13f76eb9482e3425a3dd8d821a3aabafed0d5
Solomon yusuf <solomonyusufolayinka@gmail.com>
2023-07-07 07:43:07 +0100

    remove config

--- Settings/MailSettings.cs
+++ Settings/MailSettings.cs
@@ -3,7 +3,7 @@
     public class MailSettings
     {
         public string Username { get; set; } = "yusufolayinka09@gmail.com";
-        public string Password { get; set; } = "xquayxpkfdsqmxnp";
+        public string Password { get; set; }
         public int Port { get; set; } = 587;
         public string FromEmail { get; set; } = "yusufolayinka09@gmail.com";
         public string Host { get; set; } = "smtp.gmail.com";
